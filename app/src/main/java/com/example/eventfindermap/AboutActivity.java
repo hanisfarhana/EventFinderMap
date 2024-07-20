536d36e52a2e36eed20d343f1d6a53a8c6835cdf
MSI Gaming <2022946867@student.uitm.edu.my>
2024-07-20 18:16:36 +0800

    second commit

--- app/src/main/java/com/example/eventfindermap/AboutActivity.java
+++ app/src/main/java/com/example/eventfindermap/AboutActivity.java
@@ -1,11 +1,17 @@
 package com.example.eventfindermap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +24,26 @@ public class AboutActivity extends AppCompatActivity {
                 finish(); // Close the AboutActivity and return to the previous activity
             }
         });
+
+        TextView openUrlText = findViewById(R.id.app_url);
+        openUrlText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage();
+            }
+        });
+    }
+
+    private void openWebPage() {
+        String url = "https://github.com/hanisfarhana/EventFinderMap";
+
+        // Create a Uri object from the URL
+        Uri webpage = Uri.parse(url);
+
+        // Create an Intent with action view and the Uri
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Start the activity without checking if there's an app to handle it
+        startActivity(intent);
     }
 }
